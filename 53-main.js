70f3559e76a5193e53663445c2ef9eb214eccabc
aicha652 <aichaazlf652@gmail.com>
2024-06-04 20:09:38 +0200

    Js Screen 📌

---
+++ 53-main.js
@@ -0,0 +1,13 @@
+//Screen
+//The window.screen object contains information about the user's screen.
+
+console.log(screen.width)
+console.log(screen.height)
+
+console.log(screen.availWidth)
+console.log(screen.availHeight)
+
+console.log(screen.colorDepth)
+console.log(screen.pixelDepth)
+
+console.log(screen.orientation.type)
